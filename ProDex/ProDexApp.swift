2ebea213aee3824daae4735f084e1df74ace079c
Eduardo Gonzalez Melgoza <eduardogonzalezmelgoza@10-20-56-147.0200.fed.it.iosda.org>
2023-11-14 14:31:55 +0100

    Initial commit

---
+++ ProDex/ProDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProDexApp.swift
+//  ProDex
+//
+//  Created by Eduardo Gonzalez Melgoza on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ProDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
